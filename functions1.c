3ccb8ac2ba9d7a234eca789e7844d65e7f745d34
meedeh00 <meedehtayo69@gmail.com>
2023-08-25 13:07:13 -0700

    gggg

--- functions1.c
+++ functions1.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* PRINT UNSIGNED NUMBER *************************/
+/************************* PRINT UNSIGNED NUMBERS *************************/
 /**
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
